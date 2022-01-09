8f09789b93e18f2364b53053b4278d58d15a089f
Jzjssje <58160780+Shshshhj@users.noreply.github.com>
2022-01-09 14:02:08 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "about9":
+    if data == "about":
         await query.message.edit_text(
             text = f"<u>how to use me </u>\n \nFirst sent any images Or files and then sent the below commands. \n 9/batch - create link for more than one posts.\n /genlink - create link for one post",
             disable_web_page_preview = True,
